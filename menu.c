b8ec8e752244dae200842eff5a76a9a891b5d86e
asierolaizola <asierolaizola@opendeusto.es>
2022-03-24 10:22:55 +0100

    Primer commit prueba

---
+++ menu.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void){
+	printf("Bienvenido a cines APJ\n");
+	char opcion;
+	while(opcion!='q'){
+		printf("1.Anyadir pelicula a sala\n");
+		printf("2.Anyadir cines,\n");
+		printf("3.Anyadir salas\n");
+		printf("4.Configuracion\n");
+		printf("5.Eliminar pelicula de sala\n");
+		printf("r volver\n");
+		printf("q salir\n");
+		printf("Selecciona una opcion\n");
+		scanf("%c",&opcion);
+		fflush(stdin);
+
+	}
+}
